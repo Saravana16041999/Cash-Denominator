da667d6075ef566e85cc52640f979573949f2420
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-10-10 14:22:12 +0530

    qadsfbv

--- script/script.js
+++ script/script.js
@@ -5,25 +5,53 @@ const billedEl = document.getElementById('BilledAmount');
 const paidEl = document.getElementById('PaidAmount')
 const CalculateEl = document.getElementById(`calculate`)
 const errorEl =  document.getElementById('correction')
-let change = Number(billedEl.value) - Number(paidEl.value)
 
-
-//function for calculating balance
-function balance(){
-    errorEl.textContent = 'balance is '+change
-}
 
 //function for calculating denominator
-const OldDenominator = [2000,500,100,50,10,1]
-const newDenominatoe = []
+const OldDenominator = [2000 , 500, 200, 100, 50, 20, 10, 5, 1];
+const newDenominatoe = [];
+
+function calculation(change){
+    for(i = 1; i < OldDenominator.length; i++){
+        newDenominatoe.push(Math.trunc(change / OldDenominator[i])) 
+        change = change % OldDenominator[i];  
+    }
+    console.log(newDenominatoe)
+}
+
+let valueEl = document.getElementById('value')
+let countEl = document.getElementById('count')
+function display(){
+    valueEl.className = 'value'
+    countEl.className = 'count'
+    valueEl.innerText = 'value'
+    countEl.innerText = 'count'
+    let addul = document.createElement('ul')
+    addul.clssList.add('deno')
+    sucesscontainer = document.getElementById('sucessContainer')
+    sucesscontainer.appendChild(addul);
+}
+
+
 
 
 //event Listener for calculation
 CalculateEl.addEventListener('click',()=>{
-    if (billedEl.value === '' || paidEl.value ===''){
-        errorEl.className = 'error'
+    const billamt = Number(billedEl.value)
+    const paidamt = Number(paidEl.value)
+    let Change = paidamt - billamt
+    if (billamt > 0 && paidamt > 0){
+        if(Change >= 0){
+            errorEl.className = 'sucess'
+            errorEl.textContent = `balance ${Change}`
+            calculation(Change)
+            display()
+        }else{
+            errorEl.className = 'error'
+            errorEl.textContent = 'No Balance is There '+ Change
+        }
+        
     }else{
-        errorEl.className = 'sucess'
-        balance();
+        errorEl.className = 'error'
     }
 })
